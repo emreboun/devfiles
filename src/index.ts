8cfe63b4809eeef092031dd28f6727eb6f807049
emreboun <abdurrahmanemreyilmaz@gmail.com>
2026-05-21 07:25:22 +0300

    automated-commit-4

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,27 @@ export * from "./utils/projectUtils";
 
 // If you need to expose the Express app for advanced usage
 import express from "express";
+import { FileService } from "./services/fileService";
+import { ProjectService } from "./services/projectService";
 export const createApp = (): express.Application => {
   const app = express();
   app.use(express.json());
   return app;
 };
+
+const init = async () => {
+  try {
+    const fileService = new FileService();
+    await fileService.writeFile("test.txt", "Hello World");
+
+    const projectService = new ProjectService();
+    const project = await projectService.createNewProject({
+      name: "my-project",
+      version: "1.0.0",
+    });
+    console.log(project);
+  } catch (e) {}
+};
+console.log("init-start");
+init();
+console.log("init-finish");
--- src/models/FileSystem.ts
+++ src/models/FileSystem.ts
@@ -1,22 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
 
-export interface FileNode {
-  id: string;
-  name: string;
-  path: string;
-  type: "file" | "directory";
-  content?: string;
-  children?: FileNode[];
-  metadata: {
-    size?: number;
-    lastModified?: Date;
-    language?: string;
-    dependencies?: string[];
-    imports?: string[];
-    exports?: string[];
-  };
-}
-
 export class File {
   public readonly id: string;
   public content: string;
